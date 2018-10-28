863ad8b84a601b0439c2819bec9407a5249e3ac0
Wen-Juan Ma <wenjuanma84@gmail.com>
2018-10-28 18:42:15 -0400

    add .

--- script/1Nup_ggplot.R
+++ script/1Nup_ggplot.R
@@ -11,8 +11,9 @@ N_up <- read.table("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/gi
 str(N_up)
 
 pdf(file="/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Log2FC1_MAT_a1_1nupregulate.pdf", width=7,height=5)
-ggplot(N_up, aes(x = start, y=logFC.water.diploid), cex=2) + 
-  geom_point(size = 2)+ 
+ggplot(N_up, aes(x = start, y=logFC.water.diploid,group=share), cex=2) + 
+  geom_point(size = 2, aes(color=share))+ 
+  scale_color_manual(values=c("dark grey","red")) +
   labs (x = "Gene location on MAT chromosome", y= "Log2FC(N/N+N)", size =2) +
   theme(axis.text.x = element_text(size = 12,color = "black"))  + 
   theme(axis.text.y = element_text(size = 12,color = "black")) +
@@ -23,9 +24,10 @@ dev.off()
 A2N_up <- read.table("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/A2hapdi_v1/LogFC1_A2_1npregulate.txt", header = TRUE)
 str(A2N_up)
 
-pdf(file="/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Log2FC1_MAT_a1_1nupregulate.pdf", width=7,height=5)
-ggplot(N_up, aes(x = start, y=logFC.water.diploid), cex=2) + 
-  geom_point(size = 2)+ 
+pdf(file="/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Log2FC1_MAT_a2_1nupregulate.pdf", width=7,height=5)
+ggplot(A2N_up, aes(x = start, y=logFC.water.diploid, group=share), cex=2) + 
+  geom_point(size = 2, aes(color=share))+ 
+  scale_color_manual(values=c("dark grey","red")) +
   labs (x = "Gene location on MAT chromosome", y= "Log2FC(N/N+N)", size =2) +
   theme(axis.text.x = element_text(size = 12,color = "black"))  + 
   theme(axis.text.y = element_text(size = 12,color = "black")) +
