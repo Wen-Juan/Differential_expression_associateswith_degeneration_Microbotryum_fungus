1c1b0b061cbb26cc2a0b801b57739089a02cbff9
Wen-Juan Ma <wenjuanma84@gmail.com>
2018-11-06 17:39:56 -0500

    add .

--- input/awk1.sh
+++ input/awk1.sh
@@ -1,10 +1,9 @@
-awk '
-NR==FNR{ range[$1,$2,$3]; next }
-FNR==3
+awk 'NR==FNR{ range[$1,$2,$3]; next }
+FNR==1
 {
     for(x in range) {
         split(x, check, SUBSEP); 
         if($1==check[1] && $2>=check[2] && $3<=check[3]) print $0
     }
-}    
-' BOX_NRR.txt A1_gene_location_fi.txt
+}
+' BOX_centro_A2.txt A2_gene_location_fi.txt
