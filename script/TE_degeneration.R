1daef9c3de10611086f046323a2923b2b668f224
Wen-Juan Ma <wenjuanma84@gmail.com>
2018-11-28 14:34:49 -0500

    add .

--- script/TE_degeneration.R
+++ script/TE_degeneration.R
@@ -48,6 +48,17 @@ ggplot(exp_DE, aes(x=interval, y=prop, fill=bias)) +
   theme(axis.text.x = element_text(colour="black",size=10),axis.text.y = element_text(colour="black",size=10))
 dev.off()
 
+pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Mvsl_A1A2diff_DEnonDEinterval_overlapwithTE_prop_genes.pdf", width=8, height=8)
+ggplot(exp_DE, aes(x=interval, y=diffprop, fill=bias)) + 
+  scale_fill_manual(values = c("firebrick2","dodgerblue2","grey"), labels=c("A1 bias","A2 bias", "not bias"), name="DE expression") + 
+  geom_bar(stat="identity",position=position_dodge(),alpha=0.85) +
+  ylim(-0.2,0.4) +                    
+  scale_x_discrete(labels=c("up:0-2k", "2-10k","10-20k","down:0-2k", "2-10k","10-20k")) + 
+  labs(y='Difference in prop. (A1-A2) of genes with TE insertion site') +
+  theme(axis.title.x = element_text(size=10,colour = "black"),axis.title.y = element_text(size=10,colour = "black")) +
+  theme(axis.text.x = element_text(colour="black",size=10),axis.text.y = element_text(colour="black",size=10))
+dev.off()
+
 ##A2 genome
 exp_DE_a2 <- read.table('/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/input/TE_degeneration/DEexp_TE_a2.txt', header = T)
 str(exp_DE_a2)
@@ -62,3 +73,4 @@ ggplot(exp_DE_a2, aes(x=interval, y=prop, fill=bias)) +
   theme(axis.title.x = element_text(size=10,colour = "black"),axis.title.y = element_text(size=10,colour = "black")) +
   theme(axis.text.x = element_text(colour="black",size=10),axis.text.y = element_text(colour="black",size=10))
 dev.off()
+
