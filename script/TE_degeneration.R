4bf2494b76612caf7d3e9a6665a900fc72242305
Wen-Juan Ma <wenjuanma84@gmail.com>
2018-11-29 12:37:29 -0500

    add .

--- script/TE_degeneration.R
+++ script/TE_degeneration.R
@@ -34,30 +34,31 @@ ggplot(te_interval, aes(x=interval, y=prop, fill=haploid)) +
 dev.off()
 
 #differential expression and TE insertion site
-exp_DE <- read.table('/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/input/TE_degeneration/DEexp_TE.txt', header = T)
-str(exp_DE)
+###this is wrong
+#exp_DE <- read.table('/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/input/TE_degeneration/DEexp_TE_wrong.txt', header = T)
+#str(exp_DE)
 
-pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Mvsl_A1_DEnonDEinterval_overlapwithTE_prop_genes.pdf", width=8, height=8)
-ggplot(exp_DE, aes(x=interval, y=prop, fill=bias)) + 
-  scale_fill_manual(values = c("firebrick2","dodgerblue2","grey"), labels=c("A1 bias","A2 bias", "not bias"), name="DE expression") + 
-  geom_bar(stat="identity",position=position_dodge(),alpha=0.85) +
-  ylim(0,0.5) +                    
-  scale_x_discrete(labels=c("up:0-2k", "2-10k","10-20k","down:0-2k", "2-10k","10-20k")) + 
-  labs(y='Proportion of genes with TE insertion site') +
-  theme(axis.title.x = element_text(size=10,colour = "black"),axis.title.y = element_text(size=10,colour = "black")) +
-  theme(axis.text.x = element_text(colour="black",size=10),axis.text.y = element_text(colour="black",size=10))
-dev.off()
+#pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Mvsl_A1_DEnonDEinterval_overlapwithTE_prop_genes.pdf", width=8, height=8)
+#ggplot(exp_DE, aes(x=interval, y=prop, fill=bias)) + 
+#  scale_fill_manual(values = c("firebrick2","dodgerblue2","grey"), labels=c("A1 bias","A2 bias", "not bias"), name="DE expression") + 
+#  geom_bar(stat="identity",position=position_dodge(),alpha=0.85) +
+#  ylim(0,0.5) +                    
+#  scale_x_discrete(labels=c("up:0-2k", "2-10k","10-20k","down:0-2k", "2-10k","10-20k")) + 
+#  labs(y='Proportion of genes with TE insertion site') +
+#  theme(axis.title.x = element_text(size=10,colour = "black"),axis.title.y = element_text(size=10,colour = "black")) +
+#  theme(axis.text.x = element_text(colour="black",size=10),axis.text.y = element_text(colour="black",size=10))
+#dev.off()
 
-pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Mvsl_A1A2diff_DEnonDEinterval_overlapwithTE_prop_genes.pdf", width=8, height=8)
-ggplot(exp_DE, aes(x=interval, y=diffprop, fill=bias)) + 
-  scale_fill_manual(values = c("firebrick2","dodgerblue2","grey"), labels=c("A1 bias","A2 bias", "not bias"), name="DE expression") + 
-  geom_bar(stat="identity",position=position_dodge(),alpha=0.85) +
-  ylim(-0.2,0.4) +                    
-  scale_x_discrete(labels=c("up:0-2k", "2-10k","10-20k","down:0-2k", "2-10k","10-20k")) + 
-  labs(y='Difference in prop. (A1-A2) of genes with TE insertion site') +
-  theme(axis.title.x = element_text(size=10,colour = "black"),axis.title.y = element_text(size=10,colour = "black")) +
-  theme(axis.text.x = element_text(colour="black",size=10),axis.text.y = element_text(colour="black",size=10))
-dev.off()
+#pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Mvsl_A1A2diff_DEnonDEinterval_overlapwithTE_prop_genes.pdf", width=8, height=8)
+#ggplot(exp_DE, aes(x=interval, y=diffprop, fill=bias)) + 
+  #scale_fill_manual(values = c("firebrick2","dodgerblue2","grey"), labels=c("A1 bias","A2 bias", "not bias"), name="DE expression") + 
+  #geom_bar(stat="identity",position=position_dodge(),alpha=0.85) +
+  #ylim(-0.2,0.4) +                    
+  #scale_x_discrete(labels=c("up:0-2k", "2-10k","10-20k","down:0-2k", "2-10k","10-20k")) + 
+ # labs(y='Difference in prop. (A1-A2) of genes with TE insertion site') +
+  #theme(axis.title.x = element_text(size=10,colour = "black"),axis.title.y = element_text(size=10,colour = "black")) +
+  #theme(axis.text.x = element_text(colour="black",size=10),axis.text.y = element_text(colour="black",size=10))
+#dev.off()
 
 ##A2 genome
 exp_DE_a2 <- read.table('/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/input/TE_degeneration/DEexp_TE_a2.txt', header = T)
@@ -69,6 +70,32 @@ ggplot(exp_DE_a2, aes(x=interval, y=prop, fill=bias)) +
   geom_bar(stat="identity",position=position_dodge(),alpha=0.85) +
   ylim(0,0.3) +                    
   scale_x_discrete(labels=c("up:0-2k", "2-10k","10-20k","down:0-2k", "2-10k","10-20k")) + 
+  labs(y='Proportion of genes with TE insertion site') +
+  theme(axis.title.x = element_text(size=10,colour = "black"),axis.title.y = element_text(size=10,colour = "black")) +
+  theme(axis.text.x = element_text(colour="black",size=10),axis.text.y = element_text(colour="black",size=10))
+dev.off()
+
+##this is correct
+exp_DE_cor <- read.table('/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/input/TE_degeneration/DEexp_TE_cor.txt', header = T)
+str(exp_DE_cor)
+
+pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Mvsl_a2_DEnonDEinterval_overlapwithTE_prop_genes_cor.pdf", width=8, height=8)
+ggplot(exp_DE_cor, aes(x=interval, y=prop2, fill=bias)) + 
+  scale_fill_manual(values = c("firebrick2","dodgerblue2","grey"), labels=c("A1 bias","A2 bias", "not bias"), name="DE expression") + 
+  geom_bar(stat="identity",position=position_dodge(),alpha=0.85) +
+  ylim(0,0.2) +                    
+  scale_x_discrete(labels=c("up:10-22k", "2-10k","0-2k","down:0-2k", "2-10k","10-20k")) + 
+  labs(y='Proportion of genes with TE insertion site') +
+  theme(axis.title.x = element_text(size=10,colour = "black"),axis.title.y = element_text(size=10,colour = "black")) +
+  theme(axis.text.x = element_text(colour="black",size=10),axis.text.y = element_text(colour="black",size=10))
+dev.off()
+
+pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Mvsl_a1a2diff_DEnonDEinterval_overlapwithTE_prop_genes_cor.pdf", width=8, height=8)
+ggplot(exp_DE_cor, aes(x=interval, y=diffpropcor, fill=bias)) + 
+  scale_fill_manual(values = c("firebrick2","dodgerblue2","grey"), labels=c("A1 bias","A2 bias", "not bias"), name="DE expression") + 
+  geom_bar(stat="identity",position=position_dodge(),alpha=0.85) +
+  ylim(-0.1,0.05) +                    
+  scale_x_discrete(labels=c("up:10-22k", "2-10k","0-2k","down:0-2k", "2-10k","10-20k")) + 
   labs(y='Proportion of genes with TE insertion site') +
   theme(axis.title.x = element_text(size=10,colour = "black"),axis.title.y = element_text(size=10,colour = "black")) +
   theme(axis.text.x = element_text(colour="black",size=10),axis.text.y = element_text(colour="black",size=10))
