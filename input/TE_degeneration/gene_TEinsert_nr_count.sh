3b21d5acd7f52be5874545de80ddefb62c372259
Wen-Juan Ma <wenjuanma84@gmail.com>
2018-11-29 19:40:12 -0500

    add .

--- input/TE_degeneration/gene_TEinsert_nr_count.sh
+++ input/TE_degeneration/gene_TEinsert_nr_count.sh
@@ -2,14 +2,14 @@
  grep -f Mvsl_DEnonDE_A1_genelist.txt MvSl_A1_20perc_overlap_TE_10kupstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A1_10kupstream__TE_number.txt
  grep -f Mvsl_DEnonDE_A1_genelist.txt MvSl_A1_20perc_overlap_TE_20kupstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A1_20kupstream__TE_number.txt
 
- grep -f Mvsl_DEnonDE_A1_genelist.txt MvSl_A1_20perc_overlap_TE_2downpstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A1_2kupstream_TE_number.txt &
- grep -f Mvsl_DEnonDE_A1_genelist.txt MvSl_A1_20perc_overlap_TE_10kdownstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A1_10kupstream__TE_number.txt
- grep -f Mvsl_DEnonDE_A1_genelist.txt MvSl_A1_20perc_overlap_TE_20kdownstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A1_20kupstream__TE_number.txt
+ grep -f Mvsl_DEnonDE_A1_genelist.txt MvSl_A1_20perc_overlap_TE_2kdownstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A1_2kdownstream_TE_number.txt &
+ grep -f Mvsl_DEnonDE_A1_genelist.txt MvSl_A1_20perc_overlap_TE_10kdownstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A1_10kdownstream__TE_number.txt
+ grep -f Mvsl_DEnonDE_A1_genelist.txt MvSl_A1_20perc_overlap_TE_20kdownstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A1_20kdownstream__TE_number.txt
 
  grep -f Mvsl_DEnonDE_A2_genelist.txt MvSl_A2_20perc_overlap_TE_2kupstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A2_2kupstream_TE_number.txt &
  grep -f Mvsl_DEnonDE_A2_genelist.txt MvSl_A2_20perc_overlap_TE_10kupstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A2_10kupstream__TE_number.txt
  grep -f Mvsl_DEnonDE_A2_genelist.txt MvSl_A2_20perc_overlap_TE_20kupstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A2_20kupstream__TE_number.txt
 
- grep -f Mvsl_DEnonDE_A2_genelist.txt MvSl_A2_20perc_overlap_TE_2downpstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A2_2kupstream_TE_number.txt &
- grep -f Mvsl_DEnonDE_A2_genelist.txt MvSl_A2_20perc_overlap_TE_10kdownstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A2_10kupstream_TE_number.txt
- grep -f Mvsl_DEnonDE_A2_genelist.txt MvSl_A2_20perc_overlap_TE_20kdownstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A2_20kupstream__TE_number.txt
+ grep -f Mvsl_DEnonDE_A2_genelist.txt MvSl_A2_20perc_overlap_TE_2downpstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A2_2kdownstream_TE_number.txt &
+ grep -f Mvsl_DEnonDE_A2_genelist.txt MvSl_A2_20perc_overlap_TE_10kdownstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A2_10kdownstream_TE_number.txt
+ grep -f Mvsl_DEnonDE_A2_genelist.txt MvSl_A2_20perc_overlap_TE_20kdownstream_TEs.txt | cut -f9 |less | sort | uniq -c | sort -n | tr ':' '\t' | tr ';' '\t' | sed -e $'s/ /\t/g' | cut -f4,6 > Mvsl_DEnonDE_A2_20kdownstream__TE_number.txt
