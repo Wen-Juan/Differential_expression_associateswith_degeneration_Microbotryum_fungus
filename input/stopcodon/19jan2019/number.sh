a53ecee563fe50533772ec0ef555cffe41ed209e
Wen-Juan Ma <wenjuanma84@gmail.com>
2019-01-20 21:25:34 -0500

    add .

--- input/stopcodon/19jan2019/number.sh
+++ input/stopcodon/19jan2019/number.sh
@@ -1,19 +1,41 @@
-awk '$11 == 1 && $14 == "Auto"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > auto_cdsequal.txt
-awk '$11 !=1 && $14 == "Auto"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > auto_cdsnotequal.txt
-awk '$12 == 1 && $14 == "Auto"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > auto_proteinequal.txt
-awk '$12 != 1 && $14 == "Auto"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > auto_proteinnotequal.txt
+awk '$11 == 1 && $14 == "Auto && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonauto_cdsequal.txt
+awk '$11 !=1 && $14 == "Auto" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonauto_cdsnotequal.txt
+awk '$12 == 1 && $14 == "Auto" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonauto_proteinequal.txt
+awk '$12 != 1 && $14 == "Auto" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonauto_proteinnotequal.txt
 
-awk '$11 == 1 && $14 == "bPAR"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > bPAR_cdsequal.txt
-awk '$11 !=1 && $14 == "bPAR"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > bPAR_cdsnotequal.txt
-awk '$12 == 1 && $14 == "bPAR"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > bPAR_proteinequal.txt
-awk '$12 != 1 && $14 == "bPAR"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > bPAR_proteinnotequal.txt
+awk '$11 == 1 && $14 == "bPAR" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonbPAR_cdsequal.txt
+awk '$11 !=1 && $14 == "bPAR" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonbPAR_cdsnotequal.txt
+awk '$12 == 1 && $14 == "bPAR" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonbPAR_proteinequal.txt
+awk '$12 != 1 && $14 == "bPAR" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonbPAR_proteinnotequal.txt
 
-awk '$11 == 1 && $14 == "ColorStrata"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > ColorStrata_cdsequal.txt
-awk '$11 !=1 && $14 == "ColorStrata"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > ColorStrata_cdsnotequal.txt
-awk '$12 == 1 && $14 == "ColorStrata"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > ColorStrata_proteinequal.txt
-awk '$12 != 1 && $14 == "ColorStrata"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > ColorStrata_proteinnotequal.txt
+awk '$11 == 1 && $14 == "ColorStrata" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonColorStrata_cdsequal.txt
+awk '$11 !=1 && $14 == "ColorStrata" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonColorStrata_cdsnotequal.txt
+awk '$12 == 1 && $14 == "ColorStrata" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonColorStrata_proteinequal.txt
+awk '$12 != 1 && $14 == "ColorStrata" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonColorStrata_proteinnotequal.txt
 
-awk '$11 == 1 && $14 == "OldStrata"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > OldStrata_cdsequal.txt
-awk '$11 !=1 && $14 == "OldStrata"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > OldStrata_cdsnotequal.txt
-awk '$12 == 1 && $14 == "OldStrata"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > OldStrata_proteinequal.txt
-awk '$12 != 1 && $14 == "OldStrata"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > OldStrata_proteinnotequal.txt
+awk '$11 == 1 && $14 == "OldStrata" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonOldStrata_cdsequal.txt
+awk '$11 !=1 && $14 == "OldStrata" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonOldStrata_cdsnotequal.txt
+awk '$12 == 1 && $14 == "OldStrata" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonOldStrata_proteinequal.txt
+awk '$12 != 1 && $14 == "OldStrata" && $25 == "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > nonOldStrata_proteinnotequal.txt
+
+
+
+awk '$11 == 1 && $14 == "Auto && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEauto_cdsequal.txt
+awk '$11 !=1 && $14 == "Auto" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEauto_cdsnotequal.txt
+awk '$12 == 1 && $14 == "Auto" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEauto_proteinequal.txt
+awk '$12 != 1 && $14 == "Auto" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEauto_proteinnotequal.txt
+
+awk '$11 == 1 && $14 == "bPAR" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEbPAR_cdsequal.txt
+awk '$11 !=1 && $14 == "bPAR" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEbPAR_cdsnotequal.txt
+awk '$12 == 1 && $14 == "bPAR" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEbPAR_proteinequal.txt
+awk '$12 != 1 && $14 == "bPAR" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEbPAR_proteinnotequal.txt
+
+awk '$11 == 1 && $14 == "ColorStrata" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEColorStrata_cdsequal.txt
+awk '$11 !=1 && $14 == "ColorStrata" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEColorStrata_cdsnotequal.txt
+awk '$12 == 1 && $14 == "ColorStrata" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEColorStrata_proteinequal.txt
+awk '$12 != 1 && $14 == "ColorStrata" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEColorStrata_proteinnotequal.txt
+
+awk '$11 == 1 && $14 == "OldStrata" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEOldStrata_cdsequal.txt
+awk '$11 !=1 && $14 == "OldStrata" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEOldStrata_cdsnotequal.txt
+awk '$12 == 1 && $14 == "OldStrata" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEOldStrata_proteinequal.txt
+awk '$12 != 1 && $14 == "OldStrata" && $25 != "NON"' Mvsl_a1a2_exp_cds_protein_compart.txt | wc -l > DEOldStrata_proteinnotequal.txt
