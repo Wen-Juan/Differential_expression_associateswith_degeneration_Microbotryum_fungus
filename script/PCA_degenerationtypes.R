6f80efa09c479ea75daeb402d68902cc5a342b89
Wen-Juan Ma <wenjuanma84@gmail.com>
2019-03-20 17:14:31 -0400

    add .

--- script/PCA_degenerationtypes.R
+++ script/PCA_degenerationtypes.R
@@ -7,123 +7,62 @@ library(missMDA)
 
 source("https://bioconductor.org/biocLite.R")
 biocLite("pcaMethods")
+library(pcaMethods)
+
+library(devtools)
+install_github("vqv/ggbiplot")
+library(ggbiplot)
 
 #load dataset
-PCA_5degen <- read.table ("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/input/PCA/Mvsl_common_all5degenerationtypes_sort.txt", header = TRUE)
-str(PCA_5degen)
-
-PCA_5degen_all2 <- read.table ("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/input/PCA/Mvsl_all5traits_withmissingvalues2.txt", header = TRUE)
-str(PCA_5degen_all2)
-
-PCA_5degen_all <- read.table ("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/input/PCA/Mvsl_all5traits_withmissingvalues.txt", header = TRUE)
+PCA_5degen_all <- read.table ("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/input/PCA/Mvsl_all5traits_withmissingvalues_cor.txt", header = TRUE)
 str(PCA_5degen_all)
 
 y <- glm(abs~ratioprot+GC3diff+dn+intron_nr_diff+upk5diff+upk10diff+genediff-1, data = PCA_5degen_all, family = quasi) 
 summary(y)
 
-##using variables of degeneration traits as rownames
-PCA_5degen1 <- data.frame(PCA_5degen_all[,7:25])
-str(PCA_5degen1)
-PCA_5degen2 <- data.frame(t(PCA_5degen1))
-str(PCA_5degen2)
-pcaData <- PCA_5degen2
-pca <- prcomp(pcaData, scale. = TRUE)
-summary(pca)
-
-rownames(PCA_5degen2) <- c("genediff", "upk5diff", "upk10diff", "upk15diff", "upk20diff", "down5kdiff", "down10kdiff", "down15kdiff", "down20kdiff", "ratioprot", "ratiocds", "intron_nr_diff", "intron_mean_diff", "intron_total_diff", "GC0diff", "GC3diff","dn", "ds", "dnds")
-str(PCA_5degen2)
-PCA_5degen2$types <- c("TE", "TEup", "TEup", "TEup", "TEup", "TEdown", "TEdown", "TEdown", "TEdown", "Prot", "Prot", "intron", "intron", "intron", "GC0", "GC3","dn", "ds", "dnds") 
-PCA_5degen2$type5 <- c("TE", "TE", "TE", "TE", "TE", "TE", "TE", "TE", "TE", "Prot", "Prot", "intron", "intron", "intron", "GC", "GC","dnds", "dnds", "dnds") 
-
-PCA_5degen2_i <- data.frame(pca$x, types = PCA_5degen2$types, type5 = PCA_5degen2$type5)
-str(PCA_5degen2_i)
-
-autoplot(pca, data = PCA_5degen2_i, x=1, y=2, color = 'type5', shape = 'type5', label.size = 2,size=4, ylim=c(-1,1), xlim=c(-1,1))
-autoplot(pca, data = PCA_5degen2_i, x=3, y=4,color = 'type5', shape = 'type5', label.size = 2,size=4, ylim=c(-2,2), xlim=c(-2,2))
-autoplot(pca, data = PCA_5degen2_i, x=5, y=6,color = 'type5', shape = 'type5', label.size = 2,size=4, ylim=c(-2,2), xlim=c(-2,2))
-
 ##using variables of degeneration traits as column names
-PCA_5degen1 <- as.data.frame(scale(PCA_5degen_all2[,7:25]))
+PCA_5degen1 <- as.data.frame(scale(PCA_5degen_all[,7:25]))
 str(PCA_5degen1)
+sum(is.na(PCA_5degen1))
 
-#result <- pca(CA_5degen1, method="ppca", nPcs=5, seed=NA)
-###using MCA does not seem to work
-nbdim <- estim_ncpPCA(PCA_5degen1) # estimate the number of dimensions to impute
-nb = estim_ncpPCA(PCA_5degen1,ncp.max=5)
-res.comp = imputePCA(PCA_5degen1,ncp=2)
-res.pca = PCA(res.comp$completeObs)
-resMI = MIPCA(PCA_5degen1,ncp=2, scale = TRUE, method = c("Regularized"), nboot = 100)
-plot(resMI, choice = "all", axes = c(1,2), new.plot = TRUE, main = NULL, level.conf = 0.95)
+pc <- pca(PCA_5degen1, nPcs=5, method="ppca")
+imputed <- completeObs(pc)
+
 ####
-
-pcaData <- PCA_5degen1
-pca <- prcomp(pcaData, center = TRUE, scale = TRUE)
-
+pca <- prcomp(imputed, center = TRUE, scale. =  TRUE)
 summary(pca)
 
-PCA_5degen1$gencomp <- PCA_5degen_all2[,2]
-PCA_5degen1$de2 <- PCA_5degen_all2[,6]
-str(PCA_5degen1)
+pca$gencomp <- PCA_5degen_all[,2]
+pca$de2 <- PCA_5degen_all[,6]
+str(pca)
 
+ggbiplot(mtcars.pca,ellipse=TRUE,obs.scale = 1, var.scale = 1,  labels=rownames(mtcars), groups=mtcars.country) +
+  scale_colour_manual(name="Origin", values= c("forest green", "red3", "dark blue"))+
+  ggtitle("PCA of mtcars dataset")+
+  theme_minimal()+
+  theme(legend.position = "bottom")
 
-PCA_5degen1_i <- data.frame(pca$x, de2 = PCA_5degen1$de2, gencomp = PCA_5degen1$gencomp)
-str(PCA_5degen1_i)
-
-pdf(file="/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/pca_pc1_pc2.pdf")
-autoplot(pca, data = PCA_5degen1_i, x=1, y=2, color = 'de2', shape = 'gencomp', label.size = 2,size=2, ylim=c(-1,1), xlim=c(-1,1))
+#pca1 and pca2
+pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Mvsl_pca_pca1pca2.pdf", width=8, height=8)
+ggbiplot(pca, ellipse = TRUE, circle=TRUE, obs.scale = 1, var.scale = 1, labels = colnames(pca), groups = pca$gencomp) +
+  scale_colour_manual(name="Genomic compartment", values= c("dark grey", "orange", "brown","green", "blue"))+
+  theme_minimal()+
+  theme(legend.position = "bottom")
 dev.off()
-pdf(file="/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/pca_pc3_pc4.pdf")
-autoplot(pca, data = PCA_5degen1_i, x=3, y=4, color = 'de2', shape = 'gencomp', label.size = 2,size=2, ylim=c(-1,1), xlim=c(-1,1))
+
+#pca3 and pca4
+pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Mvsl_pca_pca3pca4.pdf", width=8, height=8)
+ggbiplot(pca, ellipse = TRUE, circle=TRUE, choices=c(3,4), obs.scale = 1, var.scale = 1, labels = colnames(pca), groups = pca$gencomp) +
+  scale_colour_manual(name="Genomic compartment", values= c("dark grey", "orange", "brown","green", "blue"))+
+  theme_minimal()+
+  theme(legend.position = "bottom")
 dev.off()
-pdf(file="/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/pca_pc5_pc6.pdf")
-autoplot(pca, data = PCA_5degen1_i, x=5, y=6, color = 'de2', shape = 'gencomp', label.size = 2,size=2, ylim=c(-1,1), xlim=c(-1,1))
+
+#pca5 and pca6
+pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Haploidselection_and_dosagecompensation_in_Microbotryum/output/figures/Mvsl_pca_pca5pca6.pdf", width=8, height=8)
+ggbiplot(pca, ellipse = TRUE, circle=TRUE, choices=c(5,6), obs.scale = 1, var.scale = 1, labels = colnames(pca), groups = pca$gencomp) +
+  scale_colour_manual(name="Genomic compartment", values= c("dark grey", "orange", "brown","green", "blue"))+
+  theme_minimal()+
+  theme(legend.position = "bottom")
 dev.off()
 
-#dev.off()
-#pdf(file="/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/output/figures/alltissues_pc3pc4.pdf")
-#autoplot(pca, data = count_i, x=3, y=4, colour = 'stage', shape='sex', size=4, ylim=c(-0.3,0.3), xlim=c(-0.3,0.5))
-#dev.off()
-
-#pdf(file="/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/output/figures/alltissues_pc5pc6.pdf")
-#autoplot(pca, data = count_i, x=5, y=6, colour = 'stage', shape='sex', size=4, ylim=c(-0.3,0.5), xlim=c(-0.3,0.5))
-#dev.off()
-
-#ggplot (count_i, aes(x=PC1,y=PC2,col=stage,shape=sex)) + #this does not give variance percentage.
-#  geom_point(size=3,alpha=0.8)+
-#  scale_color_manual(values = c("orange", "red","pink","darkblue","blue", "brown", "green",  "grey"))+ 
-#  theme_set(theme_bw(base_size=12)) +
-#  theme(legend.justification=c(1,0), legend.position=c(0.95,0.05)) 
-
-
-#ggplot (count_i, aes(x=PC3,y=PC4,col=stage,shape=sex,variance_percentage = TRUE)) + #this does not give variance percentage.
-# geom_point(size=3,alpha=0.8) +
-#  scale_color_manual(values = c("orange", "red","pink","darkblue","blue", "brown", "green",  "grey"))+ 
-#  theme_set(theme_bw(base_size=12))
-
-#ggplot (count_i, aes(x=PC5,y=PC6,col=stage,shape=sex)) + #this does not give variance percentage.
-#  geom_point(size=3,alpha=0.8)+
-#  scale_color_manual(values = c("orange", "red","pink","darkblue","blue", "brown", "green",  "grey"))+ 
-#  theme_set(theme_bw(base_size=12)) +
-#  theme(legend.justification=c(1,0), legend.position=c(0.95,0.05))
-
-#pca1 <- PCA(pcaData)
-#par(mar=c(5,5,4,3))
-#plot.PCA(pca1, axes=c(1, 2), label="none", col.ind=c("orange", "orange", "orange", "orange","orange","orange","red","red","red","red","red","red","pink","pink", "pink", "pink", "pink","pink","pink","pink","pink","darkblue","darkblue", "darkblue", "darkblue","darkblue","darkblue","blue","blue","blue","blue","blue", "brown", "brown", "brown", "brown", "brown", "brown", "brown", "brown", "brown","brown", "green", "green", "green", "green", "green", "green", "green", "green", "green", "green", "grey", "grey", "grey", "grey", "grey", "grey", "grey", "grey", "grey"), title="PCA 1 vs PCA 2", cex=1.5)
-#plotellipses(plot.PCA)
-#plot.PCA(pca1, axes=c(3, 4), col.ind=c("red","red","red","blue","blue","blue","red","red","red","blue","blue","blue","red","red","red","red","blue","blue","blue","blue","blue","red","red","red","blue","blue","blue","red","red","blue","blue","blue","red","red","red","red","red","blue","blue","blue","blue","blue","red","red","red","red","red","blue","blue","blue","blue","blue","red","red","red","red","red","blue","blue","blue","blue"), title="PCA 3 vs PCA 4", cex=0.7)
-#plot.PCA(pca1, axes=c(4, 5), col.ind=c("red","red","red","blue","blue","blue","red","red","red","blue","blue","blue","red","red","red","red","blue","blue","blue","blue","blue","red","red","red","blue","blue","blue","red","red","blue","blue","blue","red","red","red","red","red","blue","blue","blue","blue","blue","red","red","red","red","red","blue","blue","blue","blue","blue","red","red","red","red","red","blue","blue","blue","blue"), title="PCA 1 vs PCA 2", cex=0.4)
-#plotellipses(pca1)
-
-#y <- dgl
-#colnames(y) <- paste(colnames(y), design$group, sep="\n")
-#cols = c(col.M23, col.F23,col.M27,col.F27, col.M31, col.F31,col.M43,col.F43,col.R43,col.M46,col.F46,col.R46)#for tvedora
-#cols = c(col.M23, col.F23,col.U23,col.M27,col.F27,col.U27, col.M31, col.F31,col.U31,col.M43,col.F43,col.M46,col.F46,col.MF46) #for argovie
-#cols = c(col.M23, col.F23,col.M27,col.F27, col.M31, col.F31,col.M43,col.F43,col.M46,col.F46,col.SR46,col.FB,col.BM,col.BF,col.FL,col.ML,col.FO,col.MT) #for ammarnas
-#pchs = c(18,5,18,5,18,5,18,5,16,18,5,16) #for tvedora
-#pchs = c(18,5,18,5,18,5,18,5,18,5,1,16,1,16,1,16,1)
-#plotMDS(y, pch=pchs[design$group],col=cols[design$group], cex=0.6, main="Ammarnas MDS plot",cex.main=0.8, cex.lab=0.5,lty=1.5, lwd=2)
-#legend('bottomright', inset=0.02, legend=levels(design$group), pch = pchs, col=cols )
-#plotMDS(y, cex=1, col=cols[design$group], main=paste(sub_analyse,"MDS plot"))
-#plotMDS(y, cex=1, pch=as.numeric(y$samples$group), col=as.numeric(y$samples$group), main=paste(sub_analyse,"MDS plot"))
-#legend('bottomright', legend=levels(design$group), pch = as.numeric(y$samples$group), col=as.numeric(y$samples$group))
-#dev.off()
